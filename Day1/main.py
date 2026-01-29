848ffec363ab532c4923f3b3ff364491e2c5ddb8
Namgit config --global user.email <divysaxena2402@gmail.com>
2026-01-29 19:25:57 +0530

    Updated

--- Day1/main.py
+++ Day1/main.py
@@ -1,38 +1,35 @@
-#User Profile validation System
+name = input("Full Name: ")
+email = input("Email: ")
+mobile = input("Mobile: ")
+age = int(input("Age: "))
 
-full_name = input("Enter Full Name: ")
-email = input("Enter Email ID: ")
-mobile = input("Enter Mobile No. ")
-age = int(input("Enter Age: "))
+nameisvalid = True
+if name[0] == " " or name[len(name) - 1] == " ":
+    nameisvalid = False
+elif name.count(" ") < 1:
+    nameisvalid = False
 
-name_valid = False
-if full_name[0]!=' ' and full_name[-1]!=' ':
-    if full_name.count(" ")>=1:
-        name_valid = True
+emailisvalid = True
+if email.count("@") != 1 or email.count(".") < 1:
+    emailisvalid = False
+elif email[0] == "@":
+    emailisvalid = False
 
-email_valid = False
-if '@' in email and "." in email:
-    if email[0] != "@":
-        email_valid = True
+mobileisvalid = True
+if len(mobile) != 10:
+    mobileisvalid = False
+elif mobile.isdigit() == False:
+    mobileisvalid = False
+elif mobile[0] == "0":
+    mobileisvalid = False
 
-mobile_valid = False
-if len(mobile) == 10 and mobile.isDigit() and mobile[0] != "0":
-    mobile_valid = True
-
-age_valid = False
+ageisvalid = True
 if age >= 18 and age <= 60:
-    age_valid = True
-
-if name_valid and email_valid and mobile_valid and age_valid:
-    print("User Profile is Valid")
+    ageisvalid = True
 else:
-    print("Invalid user Profile")
-    if not name_valid:
-        print("Invalid Name")
-    if not email_valid:
-        print("Invalid Email ID")
-    if not mobile_valid:
-        print("Invalid Mobile No.")
-    if not age_valid:
-        print("Invalid Age")
-    
+    ageisvalid = False
+
+if nameisvalid and emailisvalid and mobileisvalid and ageisvalid:
+    print("User Profile is VALID")
+else:
+    print("User Profile is INVALID")
